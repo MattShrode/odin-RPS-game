fa003f5254017ba73b3fd8b5fc0405e949e2c76b
Matt Shrode <mattshrode@gmail.com>
2022-06-21 14:03:11 -0500

    Computer's Choice
    Create functions to generate a random number and assign
    rock/paper/scissors to the computer's choice.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,59 @@
 /* Javascript */
+
+//Begin match
+
+playGame();
+
+function playGame() {
+
+     console.log(welcome());
+
+     let computerChoice = computerPlay();
+     console.log(computerChoice);
+
+}
+
+//Greet user
+
+function welcome() {
+
+    return "Let's Play Rock Paper Scissors! I'll make my selection; go ahead and make yours!";
+
+}
+
+//Make a choice for the computer
+
+function computerPlay() {
+
+    let choice = generateRandomInt(1,3);
+    switch (choice) {
+        case 1:
+            return "Rock"
+            break;
+        case 2:
+            return "Paper"
+            break;
+        case 3:
+            return "Scissors"
+            break;        
+    }
+
+}
+
+//Generate a random number for the computer's choice
+
+function generateRandomInt(min,max) {
+
+    return Math.floor(min + Math.random() * (max - min +1));
+
+}
+
+//Ask the user for their choice
+
+//Comapre the user's choice to the computer's choice
+
+//Display the winner
+
+//Incrememnt the score counter
+
+//Begin next round
